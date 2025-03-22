8bf8bd01b16258a07470050c9bd02c3b7e0c2826
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2025-03-22 17:35:53 -0300

    conexao banco
    adicionando um try/catch

--- lib/teste.dart
+++ lib/teste.dart
@@ -1,21 +1,26 @@
 import 'package:mysql_client/mysql_client.dart';
 
 void main() async {
-  final conn = await MySQLConnection.createConnection(
-    host: "localhost",
-    port: 3306,
-    userName: "root",
-    password: "password",
-    databaseName: "meubanco",
-  );
+  try {
+    final conn = await MySQLConnection.createConnection(
+      host: "localhost",
+      port: 3306,
+      userName: "root",
+      password: "password",
+      databaseName: "banco2",
+    );
 
-  await conn.connect();
-  print("✅ Conectado com sucesso!");
+    await conn.connect();
+    print("✅ Conectado com sucesso!");
 
-  var result = await conn.execute("SHOW TABLES");
-  for (final row in result.rows) {
-    print(row.colAt(0));
+    var result = await conn.execute("SHOW TABLES");
+    for (final row in result.rows) {
+      print("Tabela: ${row.colAt(0)}");
+    }
+
+    await conn.close();
+    print("🔒 Conexão encerrada.");
+  } catch (e) {
+    print("❌ Erro ao conectar ou executar consulta: $e");
   }
-
-  await conn.close();
 }
