0a2a5620f8bdf27f198e1a53c9392bd29e3e051b
Luiza-CLopes <luizacaissutti@gmail.com>
2025-03-25 11:19:52 -0300

    Criação base de dados app_flutter
    Correção na ordem para que a base de dados seja criada apois a conexão com o mysql.

--- lib/mysql/db_conection.dart
+++ lib/mysql/db_conection.dart
@@ -7,7 +7,7 @@ void main() async {
       port: 3306,
       userName: "root",
       password: "password",
-      databaseName: "app_flutter",
+      //databaseName: "app_flutter",
     );
 
     await conn.connect();
@@ -16,33 +16,20 @@ void main() async {
     // Criar o schema app_flutter se não existir
     await conn.execute("CREATE DATABASE IF NOT EXISTS app_flutter");
 
+    // Usar o banco de dados
+    await conn.execute("USE app_flutter");
+
     // Mostrar todos os databases
-    var result = await conn.execute("SHOW DATABASES");
-    for (final row in result.rows) {
-      print("Banco de Dados: ${row.colAt(0)}");
-    }
-
-    // Criar a tabela users se não existir
-    await conn.execute("""
-      CREATE TABLE IF NOT EXISTS users (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        email VARCHAR(255)
-      )
-    """);
-
-    // // Deletar tabela users
-    // await conn.execute("DROP TABLE IF EXISTS users");
-    // print("✅ Tabela users deletada!");
-
-    // Mostrar todas as tabelas
-    result = await conn.execute("SHOW TABLES");
-    for (final row in result.rows) {
-      print("Tabela: ${row.colAt(0)}");
-    }
+    //var result = await conn.execute("SHOW DATABASES");
+    //for (final row in result.rows) {
+    //  print("Banco de Dados: ${row.colAt(0)}");
+    //}
 
     await conn.close();
-  } catch (e) {
+  } 
+  catch (e) 
+  {
     print("❌ Erro ao conectar ou executar consulta: $e");
   }
+
 }
