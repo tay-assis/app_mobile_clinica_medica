e36229d094b93b68aa3b2fb593698d5ffba23973
FaelMos23 <faelramos2002@gmail.com>
2025-04-07 20:46:56 -0300

    Removida conexão com mysql da máquina e implementada conexão utilizando drift e sqlite. As modificações do sqlite agora se mantêm, o código em db_conection.dart, se executado N vezes, deve inserir e imprimir a mesma linha na tabela Addresses, N vezes.

--- lib/mysql/db_conection.dart
+++ lib/mysql/db_conection.dart
@@ -1,56 +1,44 @@
-import 'package:app_mobile_clinica_medica/mysql/models/address.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/clinic.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/doctor.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/doctorInsurance.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/doctorSchedule.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/insurance.dart';
-import 'package:app_mobile_clinica_medica/mysql/models/pacient.dart';
-import 'package:mysql_client/mysql_client.dart';
+import 'package:app_mobile_clinica_medica/mysql/database.dart'; // esse arquivo possui os imports das tabelas
 import 'package:drift/drift.dart';
 
 
 void main() async {
+  // Iniciando database Drift
+  final db = AppDatabase();
+
   try {
-    final conn = await MySQLConnection.createConnection(
-      host: "localhost",
-      port: 3306,
-      userName: "root",
-      password: "password",
-      //databaseName: "app_flutter",
-    );
 
-    await conn.connect();
-    print("✅ Conectado com sucesso!");
-
-    // Criar o schema app_flutter se não existir
-    await conn.execute("CREATE DATABASE IF NOT EXISTS app_flutter");
-
-    // Usar o banco de dados
-    await conn.execute("USE app_flutter");
-
-    // Configuração do banco de dados
-    @DriftDatabase(
-      tables: [Addresses, Clinics, Doctors, Insurances, Patients, DoctorInsurances, Doctorschedule],
-    ) 
-
-    class AppDatabase extends _$AppDatabase {
-      AppDatabase(QueryExecutor e) : super(e);
-
-  @override
-  int get schemaVersion => 1;
-}
-
-    // Mostrar todos os databases
-    //var result = await conn.execute("SHOW DATABASES");
-    //for (final row in result.rows) {
-    //  print("Banco de Dados: ${row.colAt(0)}");
+    // VERIFICACAO existencia da tabela no SQLite do Drift
+    //final tables = await db.customSelect('SELECT name FROM sqlite_master WHERE type="table"').get();
+    //for (final table in tables) {
+    //  print("Tabela: ${table.readString('name')}");
     //}
 
-    await conn.close();
+
+    // Exemplo de insercao de dados
+    print("\n\nElemento inserido na tabela Addresses:\n");
+    await db.into(db.addresses).insert(AddressesCompanion(
+      street: Value('123 Main St'),
+      neighborhood: Value('Downtown'),
+      city: Value('Metropolis'),
+      state: Value('NY'),
+      zipCode: Value(12345),
+    ));
+
+    // Query para receber todos os dados da tabela de enderecos
+    final addresses = await db.select(db.addresses).get();
+
+    // imprimir tabela de enderecos
+    for (final address in addresses) {
+      print('ID: ${address.id}, Street: ${address.street}, Neighborhood: ${address.neighborhood}, City: ${address.city}, State: ${address.state}, ZipCode: ${address.zipCode}');
+    }
   } 
   catch (e) 
   {
     print("❌ Erro ao conectar ou executar consulta: $e");
+  }finally {
+    // Ensure the database is closed after usage
+    await db.close();
   }
 
 }
