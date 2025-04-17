e21871898b46eba418d0304fa39b689bb77e66bb
FaelMos23 <faelramos2002@gmail.com>
2025-04-17 18:15:25 -0300

    Iniciando o trabalho de adicionar as DAOs, criei métodos de leitura, inserção, modificação e deleção de elementos para Address e Doctor:
    Abaixo seguem os métodos criados e o que fazem
    Address:
    - selAdd: retorna lista com todos elementos
    - selAddID: retorna o elemento com a ID requisitada
    - lenAdd: retorna número de linhas na tabela(essa tive q pegar do chat, nn entendi muito, as outras vi a documentação do Drift)
    - insAdd: insere um elemento
    - modAddID: modifica o elemento cujo ID foi solicitado (método complicado, vide p.p.s abaixo)
    - delAdd: remove todas as linhas da tabela e reseta a contagem de ID(também peguei essa parte de resetar ID do chat, não achei na documentação)
    - delAddID: deleta linha cujo ID foi solicitado
    
    Doctor:
    - selDoc: retorna lista com toda as linhas
    - selDocCli: retorna lista com todos os médicos relacionados à clínica cujo ID foi solicitado
    - selDocCRM: retorna médico cujo CRM foi solicitado
    - insDoc: insere um elemento
    - modDocCRM: modifica médico cujo CRM foi solicitado (vide p.p.s.)
    - delDocCRM: remove todas as linhas da tabela e reseta a contagem de CRM
    
    p.s.:
    Gostaria de discutir com a Luiza sobre padronização de nomes de funções e variáveis, além de uma possível modularização adicionando arquivos apenas para definições do RUD
    
    p.p.s.:
    Para os métodos de modificação, pensei em todos os elementos serem passados na chamada do método, no entanto, nem sempre a modificação é completa, então inseri flags chamadas de targets, que serão passadas também. Ex.:
    Se quer modificar a linha 13 da tabela endereços, mas somente quer-se mudar a cidade e o zipcode, portanto chama-se o método: "modAddID(13, '', 'string_ignorada', "nova_cidade", '', 54321, 0x00101)", os bits igual a 1 são o 3 e o 5, portanto, na ordem de envio, serão modificados apenas os valores que se quer.

--- lib/sqlite/db_conection.dart
+++ lib/sqlite/db_conection.dart
@@ -1,5 +1,5 @@
 import 'package:app_mobile_clinica_medica/sqlite/database.dart'; // esse arquivo possui os imports das tabelas
-import 'package:drift/drift.dart';
+
 
 
 void main() async {
@@ -15,23 +15,55 @@ void main() async {
     //}
 
 
+    //print("\n\nElemento inserido na tabela Addresses:\n");
     // Exemplo de insercao de dados
-    print("\n\nElemento inserido na tabela Addresses:\n");
-    await db.into(db.addresses).insert(AddressesCompanion(
-      street: Value('123 Main St'),
-      neighborhood: Value('Downtown'),
-      city: Value('Metropolis'),
-      state: Value('NY'),
-      zipCode: Value(12345),
-    ));
+    //await db.into(db.addresses).insert(AddressesCompanion(
+    //  street: Value('123 Main St'),
+    //  neighborhood: Value('Downtown'),
+    //  city: Value('Metropolis'),
+    //  state: Value('NY'),
+    //  zipCode: Value(12345),
+    //));
 
-    // Query para receber todos os dados da tabela de enderecos
-    final addresses = await db.select(db.addresses).get();
+    print('Criando 2 elementos iguais:');
+    // novo insert
+    await db.insAdd('Demosthenes Berbert de Castro', 'Sapetinga', 'Ilheus', 'BA', 28657);
+    await db.insAdd('Demosthenes Berbert de Castro', 'Sapetinga', 'Ilheus', 'BA', 28657);
 
-    // imprimir tabela de enderecos
-    for (final address in addresses) {
-      print('ID: ${address.id}, Street: ${address.street}, Neighborhood: ${address.neighborhood}, City: ${address.city}, State: ${address.state}, ZipCode: ${address.zipCode}');
+    // Query para receber todos os dados da tabela de enderecos e imprimir
+    final addresses1 = await db.selAdd();
+    for (final i in addresses1) {
+      print('ID: ${i.id}, Street: ${i.street}, Neighborhood: ${i.neighborhood}, City: ${i.city}, State: ${i.state}, ZipCode: ${i.zipCode}');
     }
+    print('\n');
+
+
+    int n = await db.lenAdd();
+    print('Modificando um segundo elemento ($n elementos):');
+    // modifica um elemento
+    await db.modAddID(2, 'Estância', 'Águas de Olivença', '', '', 12167, 0x11001);
+    final addr1 = await db.selAddID(1);
+    print('ID: ${addr1.id}, Street: ${addr1.street}, Neighborhood: ${addr1.neighborhood}, City: ${addr1.city}, State: ${addr1.state}, ZipCode: ${addr1.zipCode}');
+    final addr2 = await db.selAddID(2);
+    print('ID: ${addr2.id}, Street: ${addr2.street}, Neighborhood: ${addr2.neighborhood}, City: ${addr2.city}, State: ${addr2.state}, ZipCode: ${addr2.zipCode}');
+    print('\n');
+
+
+    print('Deletando elemento de ID = 1:');
+    // delete baseado no id
+    await db.delAddID(1);
+
+    n = await db.lenAdd();
+    print('Elementos sobrando: $n');
+
+    final addresses2 = await db.selAdd();
+    for (final i in addresses2) {
+      print('ID: ${i.id}, Street: ${i.street}, Neighborhood: ${i.neighborhood}, City: ${i.city}, State: ${i.state}, ZipCode: ${i.zipCode}');
+    }
+
+
+    // esvazia tabela para ter mesmos resultados sempre
+    await db.delAdd();
   } 
   catch (e) 
   {
