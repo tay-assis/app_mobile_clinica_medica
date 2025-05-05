990099a48b9881a77aaf7a8baff675485b7f531e
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2025-05-05 16:39:14 -0300

    dando commit na versao que esta supostamente funcionando, firebase esta rodando com google services

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,33 +1,13 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    //id("com.google.gms.google-services") // ADICIONANDO PARA CORRIGIR O FIREBASE 
-
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-
-    // ADICIONADO PARA CORRIGIR O FIRBASE
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    classpath("com.google.gms:google-services:4.4.1")
-}
-
-buildscrit{
-    dependencies {
-    // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
-    classpath("com.google.gms:google-services:4.4.1")
-
-
-
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
-    // https://firebase.google.com/docs/android/setup#available-libraries
-    }
+    id("com.google.gms.google-services") // plugin do Firebase
 }
 
 android {
     namespace = "com.example.app_mobile_clinica_medica"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -40,20 +20,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.app_mobile_clinica_medica"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
