0b1d7d53896b65ceae8cdf664b9e10b9af3f114f
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2025-05-28 10:30:46 -0300

    fix-correção da view dashboard_clinic.dart

--- lib/features/clinic/view/dashboard_clinic.dart
+++ lib/features/clinic/view/dashboard_clinic.dart
@@ -117,7 +117,7 @@ class _DashboardClinicState extends State<DashboardClinic> {
                         ),
                       ),
                     ),
-                    SizedBox(height: 35),
+                    SizedBox(height: 20),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: SearchInputField(),
